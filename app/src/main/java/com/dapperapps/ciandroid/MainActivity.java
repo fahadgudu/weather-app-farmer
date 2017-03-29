1d8e9a5cfe52d6e2bffebc18c84041a82f6d8d10
usmanafzal <usmanafzal@dapperapps.com.au>
2017-03-29 12:03:52 +0500

    Add fabric

--- app/src/main/java/com/dapperapps/ciandroid/MainActivity.java
+++ app/src/main/java/com/dapperapps/ciandroid/MainActivity.java
@@ -3,6 +3,8 @@ package com.dapperapps.ciandroid;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import com.crashlytics.android.Crashlytics;
+import com.crashlytics.android.core.CrashlyticsCore;
+
 import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,7 +12,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Fabric.with(this, new Crashlytics());
+        // Set up Crashlytics, disabled for debug builds
+        Crashlytics crashlyticsKit = new Crashlytics.Builder()
+                .core(new CrashlyticsCore.Builder().disabled(BuildConfig.DEBUG).build())
+                .build();
+
+        // Initialize Fabric with the debug-disabled crashlytics.
+        Fabric.with(this, crashlyticsKit);
         setContentView(R.layout.activity_main);
     }
 }
